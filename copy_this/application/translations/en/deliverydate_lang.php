c982480d283932ad9842d1cb44a09b7e71138afa
Walter Göth <63449521+wgoeth@users.noreply.github.com>
2020-04-22 12:29:35 +0200

    Add files via upload

---
+++ copy_this/application/translations/en/deliverydate_lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Delivery Date Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ * @copyright   Copyright (c) 2013 Peter Wiedeking
+ * @author      Peter Wiedeking
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
+$sLangName = 'English';
+$aLang = array(
+  'charset' => 'UTF-8',
+  'PAGE_CHECKOUT_PAYMENT_DELIVERYDATE_HEADLINE' => 'The Delivery Date is:',
+  'PAGE_CHECKOUT_PAYMENT_DELIVERYDATE_SUMMERY' => 'The delivery will be on:',
+  'PAGE_CHECKOUT_PAYMENT_DELIVERYDATE_NOORDER' => 'For today our shop is closed. Please come back on Monday. Thank you!',
+  'PAGE_CHECKOUT_PAYMENT_DELIVERYDATE_NOTTODAY' => 'You can order for tomorrow only. For todays delivery it is to late.',
+  'PAGE_CHECKOUT_ORDER_DELIVERYDATE_HEADLINE' => 'Delivery Date',
+  'EMAIL_ORDER_CUST_HTML_DELIVERYDATE' => 'The delivery will be on:',
+  'PAGE_CHECKOUT_THANKYOU_DELIVERYDATE' => 'The delivery will be on:',
+);
