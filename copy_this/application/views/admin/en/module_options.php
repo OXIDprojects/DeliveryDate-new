30bbea0c5dcb70d2bf670adb1d24254d3e53ca62
Walter Göth <63449521+wgoeth@users.noreply.github.com>
2020-04-22 12:33:26 +0200

    Add files via upload

---
+++ copy_this/application/views/admin/en/module_options.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Delivery Date Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ * @copyright   Copyright (c) 2013 Peter Wiedeking
+ * @author      Peter Wiedeking
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
+
+$sLangName  = "English";
+$aLang = array(
+'charset' => 'UTF-8',
+'SHOP_MODULE_GROUP_main' => 'Delivery Date Options',
+'SHOP_MODULE_DeadLine' => 'Latest Order-Date for todays Delivery (in full hours)',
+'SHOP_MODULE_Werktag' => 'Saturdy Working Day J/N - N is Default',
+'SHOP_MODULE_Tage' => 'Count of Days for Select (Bankholidays and not workingdays not displayed) - 11 is Default',
+);
+);
